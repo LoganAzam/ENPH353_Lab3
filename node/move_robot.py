785fe64369f3374811f9b9ba0a061624c76a1536
LoganAzam <loganazam@gmail.com>
2026-01-28 17:32:30 -0800

    Enhance documentation for move_robot.py
    Added documentation comments for the LineFollower class and its methods, explaining the purpose and functionality of the module.

--- node/move_robot.py
+++ node/move_robot.py
@@ -1,5 +1,11 @@
 #! /usr/bin/env python3
 
+## @package move_robot
+#  This module is for Lab 3 ENPH353 on line following and ROS
+#
+#  This node processes camera data from Gazebo to identify a line 
+#  on the Monza track and publishes velocity commands to follow it.
+
 import rospy
 import cv2
 import numpy as np
@@ -7,13 +13,30 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 
+## LineFollower is a class to take in video, process it and return steering instructions
+#
+#  Handles the initialization of ROS interfaces and the OpenCV logic
+#  required to calculate robot steering.
 class LineFollower:
 
+    ## The constructor.
+    #  Sets up the CV Bridge and defines the Subscriber and Publisher.
     def __init__(self):
+        ## @var bridge
+        #  An instance of CvBridge to convert ROS images to OpenCV.
         self.bridge = CvBridge()
+        
+        ## @var image_sub
+        #  The ROS subscriber for the raw camera feed.
         self.image_sub = rospy.Subscriber("/robot/camera1/image_raw", Image, self.callback, queue_size=3)
+        
+        ## @var cmd_pub
+        #  The ROS publisher for sending velocity commands to the robot.
         self.cmd_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 
+    ## Documentation for the callback method.
+    #  @param self The object pointer.
+    #  @param data The ROS image message received from the camera.
     def callback(self, data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -21,22 +44,28 @@ class LineFollower:
             rospy.logerr(e)
             return
 
+        # Processing the image to find the line
         gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
         gauss = cv2.GaussianBlur(gray, (5, 5), 0)
         
+        # Binary thresholding to isolate the black path
         _, thresh = cv2.threshold(gauss, 75, 255, cv2.THRESH_BINARY_INV)
 
+        # Moments determines the centroid of the track pixels
         moments = cv2.moments(thresh)
         move = Twist()
 
         if moments['m00'] > 0:
+            # Calculating the horizontal centroid
             cx = int(moments['m10']/moments['m00'])
             width = cv_image.shape[1]
             error = cx - width/2
             
+            # P-Controller logic
             move.linear.x = 0.2
             move.angular.z = -float(error) / 90
         else:
+            # Default behavior if the line is not in view
             move.linear.x = 0
             move.angular.z = 0.3
 
@@ -47,9 +76,10 @@ class LineFollower:
         # cv2.waitKey(1)
 
 if __name__ == '__main__':
+    ## Main execution block.
+    #  Initializes the node and instantiates the follower.
     rospy.init_node('line_follower_node')
     try:
-        # Match the class name precisely
         follower = LineFollower()
         rospy.spin()
     except rospy.ROSInterruptException:
